f8ddd30b5e1333e41eb7cacdd12c31f2fc79605d
Hasnat Mahbub <imhasnat00@gmail.com>
2022-10-12 10:40:19 +0600

    topics shown in UI

---
+++ src/components/Topic.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Topic = ({ topic }) => {
+    const { id, name, logo, total } = topic;
+
+    return (
+        <div>
+            <div className="max-w-xs rounded-md shadow-md bg-gray-100">
+                <img src={logo} alt="" className="object-cover object-center w-full rounded-t-md h-72 bg-gray-600" />
+                <div className="flex flex-col justify-between p-6 space-y-8">
+                    <div className="space-y-2">
+                        <h2 className="text-3xl font-semibold tracking-wide">{name}</h2>
+                        <p className="dark:text-gray-100">{total} Questions</p>
+                    </div>
+                    <Link to={`quiz/${id}`}>
+                        <button type="button" className="flex items-center justify-center w-full p-3 font-semibold tracking-wide rounded-md bg-violet-400 text-gray-900">Start Quiz</button>
+                    </Link>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Topic;

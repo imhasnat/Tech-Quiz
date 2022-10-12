62e673ba17fa808db890215a85b2191db938fa3b
Hasnat Mahbub <imhasnat00@gmail.com>
2022-10-12 10:39:00 +0600

    topic by topic mapping added

--- src/components/Home.js
+++ src/components/Home.js
@@ -33,14 +33,14 @@ const Home = () => {
                     </div>
                 </div>
             </div>
-            {/* <div className='w-10/12 md:w-9/12 mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 justify-center gap-6 my-10 '>
+            <div className='w-10/12 md:w-9/12 mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 justify-center gap-6 my-10 '>
                 {
                     topics.map(topic => <Topic
                         key={topic.id}
                         topic={topic}
                     ></Topic>)
                 }
-            </div> */}
+            </div>
         </div>
     );
 };

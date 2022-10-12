fb1afa59c8bf77350bf800d4eb8a63305c66db68
Hasnat Mahbub <imhasnat00@gmail.com>
2022-10-12 10:44:38 +0600

    each question show and mapping for each option added

---
+++ src/components/Question.js
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+import Option from './Option';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEye } from '@fortawesome/free-solid-svg-icons';
+
+
+const Question = ({ quiz, serial }) => {
+    const [disableButton, setDisable] = useState(false);
+    const [toggleAnswer, setToggleAnswer] = useState(true)
+    const { options, question, correctAnswer } = quiz;
+
+    const handleDisable = () => {
+        setDisable(true)
+    }
+
+    const handleToggleAnswer = () => {
+        setToggleAnswer(!toggleAnswer)
+    }
+
+    return (
+        <div className='border border-gray-300 p-3 my-6 rounded-md shadow-md'>
+            <div className='flex justify-between items-center p-2'>
+                <p className='font-bold'>Question {serial}: {question}</p>
+                <button onClick={handleToggleAnswer} className='rounded-sm m-2 '><FontAwesomeIcon icon={faEye} /></button>
+            </div>
+            <p className={`font-bold mb-6 text-center text-green-700 ${toggleAnswer ? 'hidden' : 'block'}`}>Correct Answer: {correctAnswer}</p>
+            <ToastContainer />
+            {
+                options.map((option, index) =>
+                    <Option
+                        key={index}
+                        index={index}
+                        option={option}
+                        handleDisable={handleDisable}
+                        disableButton={disableButton}
+                        correctAnswer={correctAnswer}
+                    ></Option>)
+            }
+        </div>
+    );
+};
+
+export default Question;
